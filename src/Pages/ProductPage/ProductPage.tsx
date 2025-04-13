ace60ea356435aa116ae14ec3e37b7e491cc7f80
Felipe-alv-es <brago.sama@gmail.com>
2025-04-13 17:23:52 -0300

    chore: finished navBar responsivity

--- src/Pages/ProductPage/ProductPage.tsx
+++ src/Pages/ProductPage/ProductPage.tsx
@@ -19,7 +19,6 @@ import {
 const ProductPage = () => {
   const location = useLocation();
   const { apiData, isLoading } = useApiData();
-  console.log(apiData);
   const currentPage = () => pageMap[location.pathname] ?? null;
   const [currentPageNumber, setCurrentPageNumber] = useState(1);
   const handlePageChange = (value: React.SetStateAction<number>) => {
