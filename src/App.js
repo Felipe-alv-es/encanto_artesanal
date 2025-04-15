bb72ee8cb7e91bed56a79d30af671fac683388ce
Felipe-alv-es <brago.sama@gmail.com>
2025-04-14 23:25:40 -0300

    chore: finished mobile responsivity

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ function App() {
                     <Categories />
                     <Galery />
                     <Releases />
-                    {/* <SocialMedia /> */}
-                    {/* <Footer /> */}
+                    <SocialMedia />
+                    <Footer />
                   </>
                 }
               />
