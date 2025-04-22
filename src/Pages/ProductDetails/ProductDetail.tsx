241498a58259373784f600391fc2a98354f49b6a
Felipe-alv-es <brago.sama@gmail.com>
2025-04-21 23:38:48 -0300

    chore: removed unused imports

--- src/Pages/ProductDetails/ProductDetail.tsx
+++ src/Pages/ProductDetails/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Box, Typography, useMediaQuery } from "@mui/material";
 import { Navbar, Footer } from "../index.ts";
 import useApiData from "../../Hooks/FetchApiHooks/index.tsx";
