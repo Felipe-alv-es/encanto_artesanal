4bfa90004fbc10498b08df29bd377b8dea353ec3
Felipe-alv-es <brago.sama@gmail.com>
2025-05-01 13:45:06 -0300

    chore: added copyright messsage

--- src/Pages/Footer/Footer.tsx
+++ src/Pages/Footer/Footer.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { getContainerStyles } from "./Footer.styles.ts";
 import FooterListComponent from "./Components/FooterListComponent/FooterListComponent.tsx";
 
 const Footer = () => {
   return (
-    <Box sx={getContainerStyles} id="Contato">
-      <Box>
-        <FooterListComponent />
+    <>
+      <Box sx={getContainerStyles} id="Contato">
+        <Box>
+          <FooterListComponent />
+        </Box>
       </Box>
-    </Box>
+      <Box sx={{ width: "100%", textAlign: "center" }}>
+        <Typography fontSize={12} fontStyle={"italic"}>
+          Copyright © Encanto Artesanal. Todos os direitos reservados. Todas as
+          marcas e suas imagens são de propriedade de seus respectivos donos. É
+          vedada a reprodução, total ou parcial, de qualquer conteúdo sem
+          expressa autorização.
+        </Typography>
+      </Box>
+    </>
   );
 };
 
